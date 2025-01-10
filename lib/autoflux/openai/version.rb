9e54de9f919fa403bfac67aea67fad096888cdfa
Aotokitsuruya <contact@aotoki.me>
2025-01-10 21:54:30 +0800

    chore: initialize

---
+++ lib/autoflux/openai/version.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+module Autoflux
+  module OpenAI
+    VERSION = "0.1.0"
+  end
+end
---
+++ spec/autoflux/openai_spec.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+RSpec.describe Autoflux::OpenAI do
+  it "has a version number" do
+    expect(Autoflux::OpenAI::VERSION).not_to be nil
+  end
+end
